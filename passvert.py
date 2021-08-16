d1a4dc2703af4b2eb863daaa1427f20dad6b3ec2
KornFlaXe <88982293+KornFlaXe@users.noreply.github.com>
2021-08-16 14:45:20 +0200

    Renommage de quelques variables

--- passvert.py
+++ passvert.py
@@ -8,26 +8,38 @@ from PIL            import Image
 from pyzbar         import pyzbar
 from cose.messages  import CoseMessage
 
-qr_file = sys.argv[1]
-qr_pict = Image.open(qr_file)
-qr_data = pyzbar.decode(qr_pict)[0].data
-qr_ba45 = base45.b45decode(qr_data[4:])
-qr_zlib = zlib.decompress(qr_ba45)
-qr_cose = CoseMessage.decode(qr_zlib)
-qr_cbor = cbor2.loads(qr_cose.payload)
-qr_json = json.dumps(qr_cbor, indent=3)
-qr_utf8 = qr_data.decode('utf8')
+qr_code = sys.argv[1]
+qr_pict = Image.open(qr_code)
+qr_cert = pyzbar.decode(qr_pict)[0].data.decode()
+qr_ba45 = qr_cert.replace("HC1:", "")
+qr_zlib = base45.b45decode(qr_ba45)
+qr_cose = zlib.decompress(qr_zlib)
+qr_cbor = CoseMessage.decode(qr_cose)
+qr_json = cbor2.loads(qr_cbor.payload)
+qr_dump = json.dumps(qr_json, indent=3)
 
 print()
-tableprint.banner("     Décodeur Certificat COVID Numérique UE ✧ Version 0.1a     ")
-print('\n✧ Données en BASE45 du Qr Code :\n')
-print(qr_utf8)
-print('\n✧ Données en ZLIB du Qr Code :\n')
-print(qr_ba45)
-print('\n✧ Données en COSE du Qr Code :\n')
-print(qr_zlib)
-print('\n✧ Données en CBOR du Qr Code :\n')
-print(qr_cose)
-print('\n✧ Données en JSON du Qr Code :\n')
-print(qr_json)
-print()
+tableprint.banner("     Décodeur Certificat COVID Numérique UE ✧ Version 0.2a     ")
+
+cdump = input('\n✧ Voulez-vous un dump complet du certificat ? ')
+
+if "oui".startswith(cdump.lower()):
+    print('\n✧ Données Certificat :\n')
+    print(qr_cert)
+    print('\n✧ Données en BASE45 :\n')
+    print(qr_ba45)
+    print('\n✧ Données en ZLIB :\n')
+    print(qr_zlib)
+    print('\n✧ Données en COSE :\n')
+    print(qr_cose)
+    print('\n✧ Données en CBOR :\n')
+    print(qr_cbor)
+    print('\n✧ Données en JSON :\n')
+    print(qr_json)
+    print('\n✧ Indentation JSON :\n')
+    print(qr_dump)
+    print()
+else:
+    print()
+    print(qr_dump)
+    print()
